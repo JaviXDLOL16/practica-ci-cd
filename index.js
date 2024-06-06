e31003d35f76af73dab50295884bcea4b328f04b
JaviXDLOL16 <100722991+JaviXDLOL16@users.noreply.github.com>
2024-06-06 00:37:18 -0600

    add validation

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/api/v1/welcome', (req, res) => {
-    res.send('Ahora el test pruebaaaa');
+    res.send('Ahora si ya jalo');
 });
 
 app.listen(port, () => {
